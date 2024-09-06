119296a39d8b7410fa6da43086235fa4b89a6bd8
cs777 <genius5375@gmail.com>
2024-09-06 15:52:33 +0900

    unirx setting + rhythm 노드 추가

--- CS777/Assets/Script/NodeInfo.cs
+++ CS777/Assets/Script/NodeInfo.cs
@@ -1,31 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
-public class Node
+public class NodeInfo
 {
     private int startPoint;
     private int endPoint;
-
+    private bool isClick;
     private float nodeSelect = 0;
 
-    private float bpm = 1;
+    private bool nodeFull = false;
+
+    private float bpm = 0.1f;
 
 
-    public Node(int start, int end)
+    public NodeInfo(int start, int end)
     {
 
         startPoint = start;
         endPoint = end;
     }
 
-    public void nodeIncrement()
+    private void nodeIncrement()
     {
         nodeSelect = nodeSelect + bpm;
     }
-    public void nodeDecrement()
+    private void nodeDecrement()
     {
         nodeSelect = nodeSelect - bpm;
+    }
+
+    public void nodeMove()
+    {
+        if ((nodeSelect > 100) || (nodeSelect < 0))
+        {
+            Debug.Log(" node point out of range");
+            return;
+        }
+        if (100 > nodeSelect) nodeIncrement();
+        else nodeDecrement();
+    }
+    public bool IsClickSuccess()
+    {
+        if (nodeFull) return true;
+        if ((startPoint < nodeSelect) && (nodeSelect < endPoint)) return true;
+        return false;
+    }
+
+    public void nodeReset()
+    {
+        nodeSelect = 0;
     }
 
 }
